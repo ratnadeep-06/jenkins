f7e5c8fa3680ee5e05a5dc21ea595afbdbac0b8f
Ratnadeep sali <ratnadeepsali@gmail.com>
2024-10-15 12:47:01 +0530

    Update studentwar.groovy

--- studentwar.groovy
+++ studentwar.groovy
@@ -1,42 +1,36 @@
 pipeline {
-      agent{
-        label 'dummy'    //mention the label of your webserver node provided in jenkins
-    }
-    stages {
-          stage('Pull') {
-            steps {
-                 echo "we are pulling from github"
-                 git "https://github.com/AnupDudhe/studentapp-ui"
-            }
+    agent{
+        label 'dummy'
+    } 
+    stages{
+        stage (pull){
+            steps{
+                echo "we are pulling from github"
+                git "https://github.com/AnupDudhe/studentapp-ui"
+            }   
         }
-        stage('Build') {
-            steps {
+        stage (build){
+            steps{
                 sh '''
-                sudo mvn clean package 
-                sudo apt update 
+                sudo apt update
+                sudo apt install maven -y
                 sudo apt install unzip -y
                 sudo curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.96/bin/apache-tomcat-9.0.96.zip
-                sudo unzip apache-tomcat-9.0.96.zip 
-                echo we are building source code'''
-            }
-        }
-        stage('Test') {
-            steps {
-                sh '''echo "we are testing"
+                sudo unzip apache-tomcat-9.0.96.zip
+                sudo mvn clean package
+                
                 '''
-            }
+                echo "we are building"
+            }   
         }
-        stage('Deploy') {
-            steps {
+        stage (configure){
+            steps{
                 sh '''
-                 sudo mv target/*.war  apache-tomcat-9.0.96/webapps/student.war
-                 sudo bash apache-tomcat-9.0.96/bin/catalina.sh start
-                 echo "we are deploying"
+                sudo mv target/*.war  apache-tomcat-9.0.96/webapps/student.war
+                sudo bash apache-tomcat-9.0.96/bin/catalina.sh start
                 '''
-            }
+                echo "we are configuring"
+            }   
         }
     }
 }
-
-
-//default location of your target directory /home/ubuntu/workspace
